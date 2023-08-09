7d30b2f81edabaf7667ff0428101d8cd2c3ed74d
Ferllord <alsheuski@outlook.com>
2023-08-09 21:21:40 +0300

    create product ORM models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='product_images')
+    category = models.ForeignKey(to=ProductCategory,on_delete=models.CASCADE)
