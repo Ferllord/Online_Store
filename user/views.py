52f03cc8797b6f52728fece3116e619351313652
Ferllord <alsheuski@outlook.com>
2023-08-11 17:24:20 +0300

    create UserLoginForm class

--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import User
+from .forms import UserLoginForm
 
 def login(request):
-    return render(request, 'user/login.html')
+    context = {
+        'form': UserLoginForm()
+    }
+    return render(request, 'user/login.html',context=context)
 
 
 def register(request):
