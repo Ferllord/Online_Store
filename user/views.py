011a4bf36d755d71448866598757b77b276379ee
Ferllord <alsheuski@outlook.com>
2023-08-13 13:57:55 +0300

    create logic for login and registration

--- user/views.py
+++ user/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponseRedirect
 from .models import User
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
+from django.contrib import auth
+from django.urls import reverse
 
 def login(request):
-    context = {
-        'form': UserLoginForm()
-    }
-    return render(request, 'user/login.html',context=context)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'user/login.html',context=context)
 
 
 def register(request):
-    return render(request, 'user/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+        context ={
+            'form': form
+        }
+        return render(request, 'user/register.html', context =context)
